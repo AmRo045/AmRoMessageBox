38dd53121d9207985b416349b3502e262bade5d6
AmRo <MyEmailBox045@Gmail.Com>
2019-06-01 13:52:00 +0430

    Fix effect area margin issue

--- Source/AmRoMessageDialog/WinMessageBox.xaml.cs
+++ Source/AmRoMessageDialog/WinMessageBox.xaml.cs
@@ -6,6 +6,7 @@ using MaterialDesignThemes.Wpf;
 
 namespace AmRoMessageDialog
 {
+    /// <inheritdoc cref="WinMessageBox" />
     /// <summary>
     /// Interaction logic for WinMessageBox.xaml
     /// </summary>
